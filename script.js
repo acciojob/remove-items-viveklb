6f3bc331869791cf659f6c8cd5908a27a58e7ef6
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-23 12:29:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function removecolor() {
+  const select = document.getElementById("colorSelect");
+
+  if (select.selectedIndex !== -1) {
+    select.remove(select.selectedIndex);
+  }
+}
